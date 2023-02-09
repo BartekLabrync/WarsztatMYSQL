1c60a8b605ab58f7424e493719fcb652aef4e3f7
BartekLabrync <barteklabrync7@gmail.com>
2023-02-09 20:35:59 +0100

    Connect IDE to SQL Database

--- src/main/java/pl/coderslab/App.java
+++ src/main/java/pl/coderslab/App.java
@@ -1,13 +1,19 @@
 package pl.coderslab;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws SQLException {
+
+       Connection connection = DbUtil.getConnection();
+        System.out.println(connection);
+
     }
+
 }
