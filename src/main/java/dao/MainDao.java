b668a1955b85628bf93e62a020fd110971a650c4
BartekLabrync <barteklabrync7@gmail.com>
2023-02-20 00:37:51 +0100

    Create Method read, update, delete, find all ;)

---
+++ src/main/java/dao/MainDao.java
@@ -0,0 +1,44 @@
+package dao;
+
+import entity.User;
+
+public class MainDao {
+    public static void main(String[] args) {
+
+        UserDao userDao = new UserDao();
+        User user = new User();
+        user.setUsername("Alp");
+        user.setEmail("Lopezo@87.pl");
+        user.setPassword("Prty423");
+        userDao.create(user);
+
+        User read = userDao.read(1);
+        System.out.println(read);
+
+        User userToUpdate = userDao.read(1);
+        userToUpdate.setUsername("Mort");
+        userToUpdate.setEmail("Alf@0.pl");
+        userToUpdate.setPassword("zcs43");
+        userDao.update(userToUpdate);
+
+        userDao.deleteUser(18);
+
+        User secondUser = new User();
+        secondUser.setUsername("Paweł");
+        secondUser.setEmail("Pablo@pl");
+        secondUser.setPassword("Paweł90");
+        userDao.create(secondUser);
+        User[] alluser = userDao.findAllUsers();
+        for (User users : alluser){
+            System.out.println(users);
+        }
+
+
+
+
+
+
+    }
+
+
+}
--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -4,13 +4,15 @@ package dao;
 // wyszukiwania userów po id oraz wyszukania wszystkich userów
 
 import entity.User;
+import org.mindrot.jbcrypt.BCrypt;
 import pl.coderslab.DbUtil;
 
 import java.sql.*;
+import java.util.Arrays;
 
 public class UserDao {
     private static final String CREATE_USER_QUERY =
-            " insert into users(username, email, password) Values (? ? ?) ";
+            " insert into users(username, email, password) Values (?, ?, ?) ";
 
     private static final String READ_USER_QUERY =
             "select * from users where id = ?";
@@ -28,7 +30,7 @@ public class UserDao {
         try (Connection connection = DbUtil.getConnection()) {
             PreparedStatement statement =
                     connection.prepareStatement(CREATE_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
-            statement.setString(1, user.getUserName());
+            statement.setString(1, user.getUsername());
             statement.setString(2, user.getEmail());
             statement.setString(3, hashPassword(user.getPassword()));
             statement.executeUpdate();
@@ -44,16 +46,80 @@ public class UserDao {
         }
     }
 
+    private String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
 
+    public User read(int userId) {
+        try (Connection connection = DbUtil.getConnection()) {
+            PreparedStatement statement =
+                    connection.prepareStatement(READ_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
+            statement.setInt(1, userId);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getInt("id"));
+                user.setUsername(resultSet.getString("UserName"));
+                user.setEmail(resultSet.getString("Email"));
+                user.setPassword(resultSet.getString("Password"));
+                return user;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
+    public void update(User user) {
+        try (Connection connection = DbUtil.getConnection()) {
+            PreparedStatement statement =
+                    connection.prepareStatement(UPDATE_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, user.getUsername());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, this.hashPassword(user.getPassword()));
+            statement.setInt(4, user.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
-//
-//    ResultSet rs = preStmt.getGeneratedKeys();
-//    if (rs.next()) {
-//        long id = rs.getLong(1);
-//        System.out.println("Inserted ID: " + id);
-//    }
+    public void deleteUser(int userId){
+        try (Connection connection = DbUtil.getConnection()){
+            PreparedStatement statement =
+                    connection.prepareStatement(DELETE_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
+            statement.setInt(1, userId);
+            statement.executeUpdate();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
 
+    public User[] findAllUsers (){
+        try (Connection connection = DbUtil.getConnection()){
+            User[] users = new User[0];
+            PreparedStatement statement =
+                    connection.prepareStatement(FIND_USERS_QUERY, Statement.RETURN_GENERATED_KEYS);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getInt("id"));
+                user.setUsername(resultSet.getString("UserName"));
+                user.setEmail(resultSet.getString("Email"));
+                user.setPassword(resultSet.getString("Password"));
+                users = addToArray(user, users);
+            }
+            return users;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
+    private User[] addToArray(User u, User[] users) {
+        User[] tmpUsers = Arrays.copyOf(users, users.length + 1); // Tworzymy kopię tablicy powiększoną o 1.
+        tmpUsers[users.length] = u; // Dodajemy obiekt na ostatniej pozycji.
+        return tmpUsers; // Zwracamy nową tablicę.
+    }
 
 }
